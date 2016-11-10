3e83f755ff08991a604835a01d544fd662b78bce
gormat <gormatevosyan14@gmail.com>
2016-11-10 17:42:11 +0400

    update

--- src/scripts/app3d/index.js
+++ src/scripts/app3d/index.js
@@ -1,7 +1,24 @@
 export class APP {
-    constructor () {
-        
+    constructor (params) {
+        this.domElement = params.domElement;
+        this.API = params.API;
+
+        this.getProjects();
+    }
+
+    getProjects() {
+        let filters = {
+            // skip: 0,
+            // limit: 20
+        };
+
+        this.API.getProjects('all', filters).then(
+            data => {
+                console.log(data);
+            },
+            err => {
+                console.log(err);
+            }
+        )
     }
 }
-
-
