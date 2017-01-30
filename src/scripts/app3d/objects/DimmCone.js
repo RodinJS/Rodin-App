b4e89c8e6d6cf5f7068b08043ad485df9ece36fd
Gor Matevosyan <gormatevosyan14@gmail.com>
2017-01-30 13:38:02 +0400

    RO-11 dimmcone made transparent, fixed back button

--- src/scripts/app3d/objects/DimmCone.js
+++ src/scripts/app3d/objects/DimmCone.js
@@ -1,43 +1,44 @@
 import {THREEObject} from "https://cdn.rodin.io/v0.0.1/rodinjs/sculpt/THREEObject.js";
 import {SceneManager} from 'https://cdn.rodin.io/v0.0.1/rodinjs/scene/SceneManager.js';
 import {THREE} from 'https://cdn.rodin.io/v0.0.1/vendor/three/THREE.GLOBAL.js';
-import changeParent  from 'https://cdn.rodin.io/v0.0.1/rodinjs/utils/ChangeParent.js';
 import {EVENT_NAMES} from 'https://cdn.rodin.io/v0.0.1/rodinjs/constants/constants.js';
+
+const scene = SceneManager.get();
 
 export class DimmCone extends THREEObject {
     constructor() {
-        let cone = new THREE.Mesh(new THREE.ConeGeometry(.5, .5, 5), new THREE.MeshBasicMaterial({color: 0x000000, transparent: true, opacity:.5, side: THREE.DoubleSide/*, wireframe:true*/}));
-        cone.renderOrder = 8;
-        super(cone);
+        let obj = new THREE.Mesh(new THREE.SphereGeometry(), new THREE.MeshBasicMaterial({
+            color: 0x000000,
+            transparent: true,
+            opacity: 0,
+            side: THREE.DoubleSide
+        }));
+        obj.renderOrder = 8;
+        super(obj);
         this.focusObject = null;
 
         this.on('ready', () => {
-            this.object3D.rotation.x = Math.PI / 2;
             this.raycastable = true;
         });
-        this.on(EVENT_NAMES.CONTROLLER_KEY_DOWN, () => {
-            if(this.focusObject && this.focusObject.Sculpt && this.focusObject.Sculpt.close){
+
+        this.on([EVENT_NAMES.CONTROLLER_KEY_DOWN, EVENT_NAMES.CONTROLLER_KEY_UP], (evt) => {
+            evt.stopPropagation();
+        });
+
+        this.on(EVENT_NAMES.CONTROLLER_KEY_UP, () => {
+            if (this.focusObject && this.focusObject.Sculpt && this.focusObject.Sculpt.close) {
                 this.focusObject.Sculpt.close();
             }
         });
     }
 
-    dimm(object, radius = 0) {
+    dimm(object) {
         this.focusObject = object;
-        this.object3D.rotation.set(Math.PI / 2,0,0);
-        this.object3D.position.set(0,0,0);
-        const camera = SceneManager.get().camera;
-        if(!radius){
-            object.geometry.computeBoundingSphere();
-            radius = object.geometry.boundingSphere.radius + 0.001;
-        }
-        let height = camera.getWorldPosition().distanceTo(object.getWorldPosition());
-        height *= 2;
+        this.object3D.position.set(0, scene.controls.userHeight, 0);
+        let radius = (new THREE.Vector3(0, 0, 0).distanceTo(object.getWorldPosition()));
         this.object3D.geometry.dispose();
-        this.object3D.geometry = new THREE.ConeGeometry(radius, height, 64);
-        object.add(this.object3D);
-        this.object3D.position.z = height/2 - .001;
-        //changeParent(this.object3D, SceneManager.get().scene);
+        this.object3D.geometry = new THREE.SphereGeometry(radius, 20, 20);
+        scene.add(this.object3D);
     }
 
     close() {
