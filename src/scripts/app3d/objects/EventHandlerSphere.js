2b8bbb17e195ab920beb7a35790bab3863e52ae1
Gor Matevosyan <gormatevosyan14@gmail.com>
2017-02-06 17:25:37 +0400

    event handler sphere bugfix

--- src/scripts/app3d/objects/EventHandlerSphere.js
+++ src/scripts/app3d/objects/EventHandlerSphere.js
@@ -2,6 +2,9 @@ import {THREEObject} from "https://cdn.rodin.io/v0.0.1/rodinjs/sculpt/THREEObjec
 import {THREE} from 'https://cdn.rodin.io/v0.0.1/vendor/three/THREE.GLOBAL.js';
 import {SceneManager} from 'https://cdn.rodin.io/v0.0.1/rodinjs/scene/SceneManager.js';
 import {EVENT_NAMES} from 'https://cdn.rodin.io/v0.0.1/rodinjs/constants/constants.js';
+import {MouseController} from 'https://cdn.rodin.io/v0.0.1/rodinjs/controllers/MouseController.js';
+
+import * as controllers from '../controllers';
 
 
 export class EventHandlerSphere extends THREEObject {
@@ -26,6 +29,22 @@ eventHandlerSphere.on('ready', (evt) => {
 
 eventHandlerSphere.on(EVENT_NAMES.CONTROLLER_HOVER, (evt) => {
     parent.postMessage({scroll:true}, "*");
+});
+
+eventHandlerSphere.on(EVENT_NAMES.CONTROLLER_KEY_DOWN, (evt) => {
+    if(evt.controller instanceof MouseController) {
+        evt.controller.startPropagation(EVENT_NAMES.MOUSE_DOWN);
+        evt.controller.startPropagation(EVENT_NAMES.MOUSE_UP);
+        evt.controller.startPropagation(EVENT_NAMES.MOUSE_MOVE);
+    }
+});
+
+eventHandlerSphere.on(EVENT_NAMES.CONTROLLER_KEY_UP, (evt) => {
+    if(evt.controller instanceof MouseController) {
+        evt.controller.stopPropagation(EVENT_NAMES.MOUSE_DOWN);
+        evt.controller.stopPropagation(EVENT_NAMES.MOUSE_UP);
+        evt.controller.stopPropagation(EVENT_NAMES.MOUSE_MOVE);
+    }
 });
 
 eventHandlerSphere.on(EVENT_NAMES.CONTROLLER_HOVER_OUT, (evt) => {
