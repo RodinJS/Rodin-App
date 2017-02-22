2b2bc1ed0e5272a5887340bdce28054725cb87d3
Gor Matevosyan <gormatevosyan14@gmail.com>
2017-02-23 01:59:37 +0400

    update

--- src/scripts/app3d/index.js
+++ src/scripts/app3d/index.js
@@ -194,8 +194,7 @@ function checkBackButtonVive() {
 
 window.scene = SceneManager.get();
 function checkBackButtonOculus() {
-    // console.log(SceneManager.get().webVRmanager.hmd.getShouldQuit);
-    if(SceneManager.get().webVRManager && SceneManager.get().webVRManager.hmd) console.log(SceneManager.get().webVRManager.hmd.getShouldQuit());
+    if(SceneManager.get().webVRManager && SceneManager.get().webVRManager.hmd) window.title = SceneManager.get().webVRManager.hmd.getShouldQuit();
     if(SceneManager.get().webVRManager && SceneManager.get().webVRManager.hmd && SceneManager.get().webVRManager.hmd.getShouldQuit()) {
         window.close();
     }
