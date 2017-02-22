bd5eed3aab70647bbdba2d8691eaae549f043ef5
Gor Matevosyan <gormatevosyan14@gmail.com>
2017-02-22 19:49:54 +0400

    update

--- src/scripts/app3d/index.js
+++ src/scripts/app3d/index.js
@@ -62,8 +62,7 @@ function loadMore(type) {
                                 if (evt.keyCode !== 1) return;
                             }
 
-                            alert(evt.keyCode);
-                            if (evt.controller instanceof ViveController && (evt.keyCode === 0 || evt.keyCode === 1)) {
+                            if (evt.controller instanceof ViveController && (evt.keyCode === 2 || evt.keyCode === 1)) {
                                 if (self.concentrated && projects[i].helix.center == projects[i].index) {
                                     enterProject(projects[i], API);
                                 }
